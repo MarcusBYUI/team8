c238c9642bbb6beff1de5d51235c6459310ad95c
KevinPena2022 <pen21006@byui.edu>
2022-04-27 19:19:41 -0700

    fixing Js Format

--- build/js/product.js
+++ build/js/product.js
@@ -1,1 +1,35 @@
-let products=[];function convertToJson(t){if(t.ok)return t.json();throw new Error("Bad Response")}function setLocalStorage(t,e){localStorage.setItem(t,JSON.stringify(e))}function getProductsData(){fetch("../json/tents.json").then(convertToJson).then(t=>{products=t})}function addToCart(t){const e=products.find(n=>n.Id===t.target.dataset.id);setLocalStorage("so-cart",e)}getProductsData(),document.getElementById("addToCart").addEventListener("click",addToCart);
+let products = [];
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// get tents data
+function getProductsData() {
+  fetch("../json/tents.json")
+    .then(convertToJson)
+    .then((data) => {
+      products = data;
+    });
+}
+// or should we do it this way?
+// async function getProductsDataAwait() {
+//   products = await fetch("../json/tents.json").then(convertToJson);
+// }
+
+// add to cart button event handler
+function addToCart(e) {
+  const product = products.find((item) => item.Id === e.target.dataset.id);
+  setLocalStorage("so-cart", product);
+}
+
+getProductsData();
+// add listener to Add to Cart button
+document.getElementById("addToCart").addEventListener("click", addToCart);
